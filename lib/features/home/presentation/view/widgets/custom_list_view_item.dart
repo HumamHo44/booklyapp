71c960394defdd7e84efd36ff1c412635f98ace2
HumamHo44 <humaminof@gmail>
2024-06-30 19:30:53 +0300

    add featured list view to home

--- lib/features/home/presentation/view/widgets/custom_list_view_item.dart
+++ lib/features/home/presentation/view/widgets/custom_list_view_item.dart
@@ -1,8 +1,8 @@
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class CustomLIstViewItem extends StatelessWidget {
-  const CustomLIstViewItem({super.key});
+class FeaturedLIstViewItem extends StatelessWidget {
+  const FeaturedLIstViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
