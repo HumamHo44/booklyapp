acb3e0b1203e15061324589dc4a9680f6a6615da
HumamHo44 <humaminof@gmail>
2024-10-10 08:38:48 +0300

    add custom book item and book details view

--- lib/features/home/presentation/view/widgets/custom_book_item.dart
+++ lib/features/home/presentation/view/widgets/custom_book_item.dart
@@ -1,13 +1,12 @@
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class FeaturedLIstViewItem extends StatelessWidget {
-  const FeaturedLIstViewItem({super.key});
+class CustomBookImage extends StatelessWidget {
+  const CustomBookImage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
       child: AspectRatio(
         aspectRatio: 2.7 / 4,
         child: Container(
