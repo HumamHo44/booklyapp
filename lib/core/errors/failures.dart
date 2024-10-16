17e8e7bd149c7c0ff66b88dd22887b98d96b671b
HumamHo44 <humaminof@gmail>
2024-10-16 08:16:18 +0300

    create Server failure from dio error

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,3 +1,47 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String errMessage;
+
+  Failure(this.errMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errMessage);
+
+  factory ServerFailure.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection timeout with ApiServer');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send timeout with ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive timeout with ApiServer');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('The certificate of the response is not approved');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+            dioError.response!.statusCode!, dioError.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailure('Request to ApiServer was canceld');
+
+      case DioExceptionType.connectionError:
+        return ServerFailure('The connection errored: No Internet Connection');
+      case DioExceptionType.unknown:
+        return ServerFailure('Unexpected error, please try Again!');
+      default:
+        return ServerFailure('Opps there was an Error,Please try again');
+    }
+  }
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure('Your request not found, please try later!');
+    } else if (statusCode == 500) {
+      return ServerFailure('Internal Server error, please try later');
+    } else {
+      return ServerFailure('Opps there was an Error,Please try again');
+    }
+  }
+}
