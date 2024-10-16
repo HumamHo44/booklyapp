d15e34ba39fcd987d49ca822fa347fcb71d1bc67
HumamHo44 <humaminof@gmail>
2024-10-16 09:08:45 +0300

    newest books cubit done

--- lib/features/home/presentation/manger/newest_books_cubit/newser_books_cubit.dart
+++ lib/features/home/presentation/manger/newest_books_cubit/newser_books_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newser_books_state.dart';
 
 class NewserBooksCubit extends Cubit<NewserBooksState> {
-  NewserBooksCubit() : super(NewserBooksInitial());
+  NewserBooksCubit(this.homeRepo) : super(NewserBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchNewestBooks() async {
+    emit(NewserBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) {
+        emit(NewserBooksFailure(failure.errMessage));
+      },
+      (books) {
+        emit(NewserBooksSuccess(books));
+      },
+    );
+  }
 }
